efefb377df82afb494fe34adb4150a9456625c27
Dev <lightmorning46@gmail.com>
2017-11-23 23:16:13 +0100

    updating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { TermsOfUsePage } from '../pages/terms-of-use/terms-of-use';
+import { PrivacyPolicyPage } from '../pages/privacy-policy/privacy-policy';
 import { AboutPage } from '../pages/about/about';
 
 export interface PageInterface {
@@ -23,7 +24,7 @@ export class MyApp {
   appPages: PageInterface[] = [
         { title: 'Home', description: 'Home', component: HomePage, icon: 'home' },
         { title: 'Terms', description: 'Terms of Use', component: TermsOfUsePage, icon: 'md-contract' },
-        { title: 'Policy', description: 'Privacy Policy', component: HomePage, icon: 'eye-off' },
+        { title: 'Policy', description: 'Privacy Policy', component: PrivacyPolicyPage, icon: 'eye-off' },
         { title: 'Feedback', description: 'Feed back', component: HomePage, icon: 'star' },
         { title: 'About', description: 'About page', component: AboutPage, icon: 'information-circle' }
     ];
