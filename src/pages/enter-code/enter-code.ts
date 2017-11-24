53a699d4d5f1d46400c29f554e475c86d70e2a9d
Dev <lightmorning46@gmail.com>
2017-11-25 03:34:21 +0900

    updating

--- src/pages/enter-code/enter-code.ts
+++ src/pages/enter-code/enter-code.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, MenuController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, MenuController, Content, Platform } from 'ionic-angular';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 import { Rest } from '../../providers/rest';
 
@@ -12,11 +15,22 @@ import { DateOfBirthPage } from '../date-of-birth/date-of-birth';
   templateUrl: 'enter-code.html',
 })
 export class EnterCodePage {
+  @ViewChild(Content) content: Content;
   scode: string;
+  dlg: any;
   constructor(public navCtrl: NavController, 
         public navParams: NavParams, 
-        public menu: MenuController, public rest: Rest) {
+        public http: Http, private sanitizer: DomSanitizer,
+        public menu: MenuController, public rest: Rest,
+        public platform: Platform) {
     this.menu = menu;
+
+    this.dlg['show'] = 0;
+    this.dlg['maxWidth'] = 600;
+    this.dlg['left'] = 0;
+    this.dlg['top'] = 0;
+    this.dlg['width'] = 200;
+    this.dlg['height'] = 100;
   }
 
   ionViewDidLoad() {
@@ -25,10 +39,30 @@ export class EnterCodePage {
   showMenu() {
     this.menu.open();
   }
+  toggleDlg(b: number)
+  {
+    if (b != 0)
+    {
+      var scrollPos = this.content.getContentDimensions().scrollTop;
+      if (b == 1)
+        this.dlg['height'] = 400;
+      else
+        this.dlg['height'] = 100;
+      this.dlg['width'] = this.platform.width() * 0.9;
+      if (this.dlg['width'] > this.dlg['maxWidth'])
+        this.dlg['width'] = this.dlg['maxWidth'];
+      this.dlg['left'] = (this.platform.width() - this.dlg['width']) / 2;
+      this.dlg['top'] = (this.platform.height() - this.dlg['height']) / 2 + scrollPos - 60;
+    }
+    this.dlg['show'] = 1;
+  }
   goCancel() {
   	this.navCtrl.push(HomePage);
   }
   goContinue() {
-  	this.navCtrl.push(DateOfBirthPage, {'code': this.scode});
+    if (this.scode == "")
+      this.toggleDlg(1);
+    else
+  	 this.navCtrl.push(DateOfBirthPage, {'code': this.scode});
   }
 }
