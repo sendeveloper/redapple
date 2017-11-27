f6255d68adbfee21258b87ecca2bcadf7a259fb2
Dev <lightmorning46@gmail.com>
2017-11-27 17:46:25 +0100

    updating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+// import { Push, PushToken } from '@ionic/cloud-angular';
+
 import { HomePage } from '../pages/home/home';
 import { TermsOfUsePage } from '../pages/terms-of-use/terms-of-use';
 import { PrivacyPolicyPage } from '../pages/privacy-policy/privacy-policy';
@@ -29,7 +31,8 @@ export class MyApp {
         { title: 'Policy', description: 'Privacy Policy', component: PrivacyPolicyPage, icon: 'eye-off' },
         { title: 'About', description: 'About page', component: AboutPage, icon: 'information-circle' }
     ];
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public rest: Rest) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, 
+        public rest: Rest) {
     platform.ready().then(() => {
       statusBar.styleDefault();
       splashScreen.hide();
@@ -41,8 +44,25 @@ export class MyApp {
       {
           this.rest.setDeviceNumber(0);
       }
+      // this.registerPush();
     });
   }
+  // registerPush() {
+  //     // Check that we are on a device
+  //   if (this.platform.is('cordova')) {
+  //       // Register push notifications with the push plugin
+  //     this.push.register().then((t: PushToken) => {
+  //       console.log('Generated Token' + JSON.stringify(t));
+  //       // Save the user with Ionic's user auth service
+  //       return this.push.saveToken(t);
+  //     }).then( (t: PushToken) => {
+  //       console.log('Token Saved', t);
+  //       this.listenForPush();
+  //     }).catch( (err) => {
+  //       console.log('Error Saving Token: ' , err);
+  //     });
+  //    }
+  // }
   openPage(page: PageInterface) {
     this.nav.setRoot(page.component).catch(() => {
     });
