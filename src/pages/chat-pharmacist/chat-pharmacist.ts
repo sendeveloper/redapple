2afcb8d030811f976bfe787f183686fd0281b38f
Dev <lightmorning46@gmail.com>
2017-12-01 11:54:40 +0900

    updating

--- src/pages/chat-pharmacist/chat-pharmacist.ts
+++ src/pages/chat-pharmacist/chat-pharmacist.ts
@@ -9,12 +9,19 @@ import { Rest } from '../../providers/rest';
   templateUrl: 'chat-pharmacist.html',
 })
 export class ChatPharmacistPage {
-
+  tabBarElement: any;
   constructor(public navCtrl: NavController, public navParams: NavParams,
   			public menu: MenuController, public rest: Rest) {
   	this.menu = menu;
+    this.tabBarElement = document.querySelector('.tabbar.show-tabbar');
   }
-
+  ionViewWillEnter() {
+    if (this.rest.getNdc() == '')
+      this.tabBarElement.style.display = 'none';
+  }
+  ionViewWillLeave() {
+    this.tabBarElement.style.display = 'flex';
+  }
   ionViewDidLoad() {
     
   }
